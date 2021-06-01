ad7cc90e731e8136e4ae4cee79e430c3414a7dba
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-01 14:37:24 +0300

    Refactoring the ids of widgets.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/MainActivity.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/MainActivity.java
@@ -3,12 +3,25 @@ package com.ivanboyukliev.plantsirrigationsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userEmail, password;
+    private Button signUpBtn;
+    private TextView signUpTv;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        userEmail = findViewById(R.id.emailInputEditText);
     }
 }
