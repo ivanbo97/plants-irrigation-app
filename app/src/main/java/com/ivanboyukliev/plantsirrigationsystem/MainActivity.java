6264ff619b6d6ef004f2d639fdb3c9f7ad37e9f0
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-01 16:04:57 +0300

    Adding user registration with Firebase platform.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/MainActivity.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/MainActivity.java
@@ -1,27 +1,75 @@
 package com.ivanboyukliev.plantsirrigationsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants;
+
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.INCORRECT_INPUT_MESSAGE;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.REGISTRATION_ERROR_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText userEmail, password;
+    private EditText userEmail, userPassword;
     private Button signUpBtn;
-    private TextView signUpTv;
-    FirebaseAuth mFirebaseAuth;
+
+    private TextView signInTv;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateWidgetObjects();
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        userEmail = findViewById(R.id.emailInputEditText);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity.this,INCORRECT_INPUT_MESSAGE,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                firebaseAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful()){
+                                    String errorMesssage = task.getException().getMessage();
+                                    Toast.makeText(MainActivity.this,errorMesssage,Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                            }
+                        });
+            }
+        });
+
     }
+
+
+    private void populateWidgetObjects(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        userEmail = findViewById(R.id.emailInputEditTextMainActivity);
+        userPassword = findViewById(R.id.passwordInputEditTextMainActivity);
+        signUpBtn = findViewById(R.id.signUpButton);
+        signInTv = findViewById(R.id.signInTv);
+    }
+
+
 }
---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package com.ivanboyukliev.plantsirrigationsystem.utils;
+
+public class ApplicationConstants {
+    public static final String INCORRECT_INPUT_MESSAGE = "Please make sure all fields are filled out!";
+    public static final String REGISTRATION_ERROR_MESSAGE = "Unsuccessful attempt for signing up! Please try again";
+}
