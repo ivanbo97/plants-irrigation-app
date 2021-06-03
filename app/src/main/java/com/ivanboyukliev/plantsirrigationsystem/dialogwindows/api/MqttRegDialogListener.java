5dbe397d29d0d82c53b2f98647aa89b2b8e40b2d
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-03 14:16:28 +0300

    Adding interface for defining actions after broker registration.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/dialogwindows/api/MqttRegDialogListener.java
@@ -0,0 +1,8 @@
+package com.ivanboyukliev.plantsirrigationsystem.dialogwindows.api;
+
+import com.ivanboyukliev.plantsirrigationsystem.utils.BasicMqttbroker;
+
+public interface MqttRegDialogListener {
+
+     void createBrokerAsWidget(BasicMqttbroker mqttbroker);
+}
