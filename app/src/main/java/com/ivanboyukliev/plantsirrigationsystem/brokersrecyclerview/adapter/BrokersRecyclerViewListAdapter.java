b4bbd6cf2827276119cd27cc356e5459874e14ef
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-04 17:36:16 +0300

    Adding event handler for connect button click.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/brokersrecyclerview/adapter/BrokersRecyclerViewListAdapter.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/brokersrecyclerview/adapter/BrokersRecyclerViewListAdapter.java
@@ -1,6 +1,7 @@
 package com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.adapter;
 
 import android.annotation.SuppressLint;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +12,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ivanboyukliev.plantsirrigationsystem.R;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBroker;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.viewholder.MqttBrokerViewHolder;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
 
 import java.util.List;
 
 public class BrokersRecyclerViewListAdapter extends RecyclerView.Adapter<MqttBrokerViewHolder> {
 
     private List<BasicMqttBroker> mqttBrokers;
+    private List<MqttClientActions> mqttClientActionsList;
 
-    public BrokersRecyclerViewListAdapter(List<BasicMqttBroker> mqttBrokers) {
+    public BrokersRecyclerViewListAdapter(List<BasicMqttBroker> mqttBrokers, List<MqttClientActions> mqttClientActionsList) {
         this.mqttBrokers = mqttBrokers;
+        this.mqttClientActionsList = mqttClientActionsList;
     }
 
     @NonNull
@@ -39,7 +43,12 @@ public class BrokersRecyclerViewListAdapter extends RecyclerView.Adapter<MqttBro
         holder.getDeleteBrokerButton().setOnClickListener(v -> {
             //For future we should delete record from DB also!!!!
             mqttBrokers.remove(position);
+            mqttClientActionsList.remove(position);
             notifyDataSetChanged();
+        });
+
+        holder.getConnectBtn().setOnClickListener(v -> {
+            mqttClientActionsList.get(position).connectClient();
         });
     }
 
