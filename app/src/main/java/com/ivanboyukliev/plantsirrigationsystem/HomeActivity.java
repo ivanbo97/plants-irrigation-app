0b5b7b9fb7e8c49da1009ebb456d45c717dc7f64
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-04 09:56:40 +0300

    Disable nav bar appearance.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/HomeActivity.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/HomeActivity.java
@@ -5,8 +5,11 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -16,6 +19,10 @@ import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.adapter.Brok
 import com.ivanboyukliev.plantsirrigationsystem.dialogwindows.MqttBrokerRegDialog;
 import com.ivanboyukliev.plantsirrigationsystem.dialogwindows.api.MqttRegDialogListener;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBroker;
+import com.ivanboyukliev.plantsirrigationsystem.utils.AndroidUIManager;
+import com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants;
+
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.NAV_BAR_INVISIBLE;
 
 public class HomeActivity extends AppCompatActivity implements MqttRegDialogListener {
 
@@ -24,11 +31,16 @@ public class HomeActivity extends AppCompatActivity implements MqttRegDialogList
     private FloatingActionButton registerBrokerBtn;
     private RecyclerView brokersListRecyclerView;
     private BrokersRecyclerViewListAdapter brokersAdapter;
+    private AndroidUIManager uiManager;
     private static BasicMqttBroker newMqttBroker;
 
+
     @Override
+    @SuppressLint("NewApi")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        uiManager = new AndroidUIManager(getWindow());
+        uiManager.disableNavigationBar();
         setContentView(R.layout.activity_home);
         firebaseAuth = FirebaseAuth.getInstance();
         newMqttBroker = new BasicMqttBroker();
@@ -48,6 +60,7 @@ public class HomeActivity extends AppCompatActivity implements MqttRegDialogList
         });
     }
 
+
     @Override
     public void onDialogDataSending() {
         brokersAdapter.notifyDataSetChanged();
@@ -57,6 +70,15 @@ public class HomeActivity extends AppCompatActivity implements MqttRegDialogList
         logoutBtn = findViewById(R.id.logoutBtn);
         registerBrokerBtn = findViewById(R.id.deleteBrokerButton);
         brokersListRecyclerView = findViewById(R.id.brokersListRecyclerView);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(NAV_BAR_INVISIBLE);
+        }
     }
 
     private void openBrokerRegisterDialog() {
