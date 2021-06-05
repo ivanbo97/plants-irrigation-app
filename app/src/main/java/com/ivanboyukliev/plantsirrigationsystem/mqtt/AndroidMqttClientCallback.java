e97538d81375803e0d0d014ec2f92d96852528d8
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-05 10:56:16 +0300

    Indicate the adapter for connect/disconnect events.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/AndroidMqttClientCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/AndroidMqttClientCallback.java
@@ -2,20 +2,33 @@ package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 
 import android.util.Log;
 
+import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.impl.MqttClientActionsImpl;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class AndroidMqttClientCallback implements MqttCallbackExtended {
 
+    private MqttClientActions disconnectAction;
+
+    public AndroidMqttClientCallback(MqttClientActions disconnectAction) {
+        this.disconnectAction = disconnectAction;
+    }
+
     @Override
     public void connectComplete(boolean reconnect, String serverURI) {
         Log.i("MQTT CLIENT STATUS:  ", "CONNECTED");
+
     }
 
     @Override
     public void connectionLost(Throwable cause) {
         Log.i("MQTT CLIENT STATUS:  ", "DISCONNECTED");
+        ((MqttClientActionsImpl) disconnectAction).setConnected(false);
+        HomeActivity.getBrokersAdapter().notifyDataSetChanged();
     }
 
     @Override
