3ab81db934474453449401435f4e38a98a005cae
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-05 10:59:41 +0300

    Adding notification for adapter after connection success/failure.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
@@ -3,19 +3,32 @@ package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 import android.util.Log;
 
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.impl.MqttClientActionsImpl;
+
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 
 public class ConnectionTokenCallback implements IMqttActionListener {
 
+    private MqttClientActions connectAction;
+
+    public ConnectionTokenCallback(MqttClientActions connectAction) {
+        this.connectAction = connectAction;
+    }
+
     @Override
     public void onSuccess(IMqttToken asyncActionToken) {
         Log.i("BROKER INFO", "Connection success!!");
+        ((MqttClientActionsImpl) connectAction).setConnected(true);
+        HomeActivity.getBrokersAdapter().notifyDataSetChanged();
     }
 
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.i("BROKER INFO", "Connection failed!!");
+        ((MqttClientActionsImpl) connectAction).setConnected(false);
         HomeActivity.showBrokerConnectionError();
+        HomeActivity.getBrokersAdapter().notifyDataSetChanged();
     }
 }
