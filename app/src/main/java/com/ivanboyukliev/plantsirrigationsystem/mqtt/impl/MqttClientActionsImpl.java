26c2e53f987441166f75ffae36a4cb86e7c43c00
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-05 11:01:29 +0300

    Implementing logic related to broker's disconnecting.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/impl/MqttClientActionsImpl.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/impl/MqttClientActionsImpl.java
@@ -6,6 +6,7 @@ import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBroker;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.AndroidMqttClientCallback;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.ConnectionTokenCallback;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.DisconnectTokenCallback;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -14,11 +15,14 @@ import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+import java.util.List;
+
 public class MqttClientActionsImpl implements MqttClientActions {
 
     private MqttAndroidClient mqttAndroidClient;
     private BasicMqttBroker mqttBroker;
     private MqttCallbackExtended mqttCallback;
+    private boolean isConnected;
 
     public MqttClientActionsImpl(BasicMqttBroker mqttBroker) {
         this.mqttBroker = mqttBroker;
@@ -31,7 +35,7 @@ public class MqttClientActionsImpl implements MqttClientActions {
         String brokerURI = mqttBroker.getBrokerIp() + ":" + mqttBroker.getBrokerPort();
         Log.i("BROKER ADDRESS",brokerURI);
         mqttAndroidClient = new MqttAndroidClient(HomeActivity.getHomeActivityContext(), brokerURI, clientId);
-        mqttCallback = new AndroidMqttClientCallback();
+        mqttCallback = new AndroidMqttClientCallback(this);
         mqttAndroidClient.setCallback(mqttCallback);
     }
 
@@ -39,11 +43,36 @@ public class MqttClientActionsImpl implements MqttClientActions {
     public void connectClient() {
 
         try {
-            ConnectionTokenCallback connectionTokenCallback = new ConnectionTokenCallback();
+            ConnectionTokenCallback connectionTokenCallback = new ConnectionTokenCallback(this);
             IMqttToken connectionToken =  mqttAndroidClient.connect();
             connectionToken.setActionCallback(connectionTokenCallback);
         } catch (MqttException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void disconnectClient() {
+        try {
+            DisconnectTokenCallback disconnectTokenCallback = new DisconnectTokenCallback(this);
+            IMqttToken disconnectionToken = mqttAndroidClient.disconnect();
+            disconnectionToken.setActionCallback(disconnectTokenCallback);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnectAllClients (List<MqttClientActions> clientActions){
+        for(MqttClientActions clientAction : clientActions){
+            clientAction.disconnectClient();
+        }
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
     }
 }
