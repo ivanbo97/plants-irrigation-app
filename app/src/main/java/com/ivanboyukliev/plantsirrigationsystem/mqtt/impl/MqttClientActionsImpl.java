880e06057db03bf1cac0f6b75c1ada63f8750639
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-06 15:45:49 +0300

    Adding SSL security support for encrypting messages between broker and device.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/impl/MqttClientActionsImpl.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/impl/MqttClientActionsImpl.java
@@ -3,19 +3,26 @@ package com.ivanboyukliev.plantsirrigationsystem.mqtt.impl;
 import android.util.Log;
 
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
+import com.ivanboyukliev.plantsirrigationsystem.R;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBroker;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.AndroidMqttClientCallback;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.ConnectionTokenCallback;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.DisconnectTokenCallback;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
+import com.ivanboyukliev.plantsirrigationsystem.ssl.SSLConfigurator;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+
+import java.io.InputStream;
 import java.util.List;
+
+import javax.net.ssl.SSLSocketFactory;
 
 public class MqttClientActionsImpl implements MqttClientActions {
 
@@ -33,7 +40,7 @@ public class MqttClientActionsImpl implements MqttClientActions {
     public void initClientData() {
         String clientId = MqttClient.generateClientId();
         String brokerURI = mqttBroker.getBrokerIp() + ":" + mqttBroker.getBrokerPort();
-        Log.i("BROKER ADDRESS",brokerURI);
+        Log.i("BROKER ADDRESS", brokerURI);
         mqttAndroidClient = new MqttAndroidClient(HomeActivity.getHomeActivityContext(), brokerURI, clientId);
         mqttCallback = new AndroidMqttClientCallback(this);
         mqttAndroidClient.setCallback(mqttCallback);
@@ -43,8 +50,20 @@ public class MqttClientActionsImpl implements MqttClientActions {
     public void connectClient() {
 
         try {
-            ConnectionTokenCallback connectionTokenCallback = new ConnectionTokenCallback(this);
-            IMqttToken connectionToken =  mqttAndroidClient.connect();
+            ConnectionTokenCallback connectionTokenCallback = new ConnectionTokenCallback(this, mqttAndroidClient);
+            MqttConnectOptions options = new MqttConnectOptions();
+
+            try {
+                InputStream caCrtFile = HomeActivity.getHomeActivityContext().getResources().openRawResource(R.raw.ca);
+                InputStream crtFile = HomeActivity.getHomeActivityContext().getResources().openRawResource(R.raw.cert);
+                InputStream keyFile = HomeActivity.getHomeActivityContext().getResources().openRawResource(R.raw.key);
+                SSLSocketFactory sslSocketFactory = SSLConfigurator.getSocketFactory(caCrtFile, crtFile, keyFile, "");
+                options.setSocketFactory(sslSocketFactory);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+            IMqttToken connectionToken = mqttAndroidClient.connect(options);
             connectionToken.setActionCallback(connectionTokenCallback);
         } catch (MqttException e) {
             e.printStackTrace();
@@ -62,8 +81,8 @@ public class MqttClientActionsImpl implements MqttClientActions {
         }
     }
 
-    public static void disconnectAllClients (List<MqttClientActions> clientActions){
-        for(MqttClientActions clientAction : clientActions){
+    public static void disconnectAllClients(List<MqttClientActions> clientActions) {
+        for (MqttClientActions clientAction : clientActions) {
             clientAction.disconnectClient();
         }
     }
