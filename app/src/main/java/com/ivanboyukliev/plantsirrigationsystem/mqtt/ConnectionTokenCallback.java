10c38d8f60b8804a15469bcd2bd76ef668f525b4
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-06 15:51:41 +0300

    Subscribe device to specific topic (only for debug purpose, fot now).

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
@@ -1,20 +1,25 @@
 package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 
+
 import android.util.Log;
 
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.impl.MqttClientActionsImpl;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class ConnectionTokenCallback implements IMqttActionListener {
 
     private MqttClientActions connectAction;
+    private MqttAndroidClient mqttAndroidClient;
 
-    public ConnectionTokenCallback(MqttClientActions connectAction) {
+    public ConnectionTokenCallback(MqttClientActions connectAction, MqttAndroidClient androidClient) {
         this.connectAction = connectAction;
+        this.mqttAndroidClient = androidClient;
     }
 
     @Override
@@ -22,11 +27,20 @@ public class ConnectionTokenCallback implements IMqttActionListener {
         Log.i("BROKER INFO", "Connection success!!");
         ((MqttClientActionsImpl) connectAction).setConnected(true);
         HomeActivity.getBrokersAdapter().notifyDataSetChanged();
+        TopicSubscriptionCallBack topicSubscriptionCallBack = new TopicSubscriptionCallBack();
+        try {
+            mqttAndroidClient.subscribe("house/bulb1", 0, null, topicSubscriptionCallBack);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.i("BROKER INFO", "Connection failed!!");
+        Log.i("REASON", exception.getMessage());
+        exception.printStackTrace();
         ((MqttClientActionsImpl) connectAction).setConnected(false);
         HomeActivity.showBrokerConnectionError();
         HomeActivity.getBrokersAdapter().notifyDataSetChanged();
