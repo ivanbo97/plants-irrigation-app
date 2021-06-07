bca9b8c56de717f43cadeaf6cca3043757e734a4
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-07 16:03:31 +0300

    New class for handling user credentials input.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/dialogwindows/MqttAuthenticationDialog.java
@@ -0,0 +1,63 @@
+package com.ivanboyukliev.plantsirrigationsystem.dialogwindows;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.ivanboyukliev.plantsirrigationsystem.R;
+import com.ivanboyukliev.plantsirrigationsystem.dialogwindows.api.MqttCredentialsInputListener;
+
+public class MqttAuthenticationDialog extends AppCompatDialogFragment {
+
+    private View dialogView;
+    private EditText username;
+    private EditText password;
+    private MqttCredentialsInputListener credentialsInputListener;
+    private int brokerNum;
+
+    public MqttAuthenticationDialog(int brokerNum) {
+        this.brokerNum = brokerNum;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        dialogView = inflater.inflate(R.layout.mqtt_broker_auth, null);
+        populateDialogWidgets();
+        dialogBuilder.setView(dialogView)
+                .setTitle("Enter Credentials")
+                .setNegativeButton("Cancel", (dialog, which) -> {
+                    //no-action needed for now
+                })
+                .setPositiveButton("Confirm", (dialog, which) -> {
+                    credentialsInputListener.onCredentialsEntered(username.getText().toString(), password.getText().toString(), brokerNum);
+                });
+        return dialogBuilder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            credentialsInputListener = (MqttCredentialsInputListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement MqttCredentailsInputListener");
+        }
+    }
+
+    private void populateDialogWidgets() {
+        username = dialogView.findViewById(R.id.mqtt_username);
+        password = dialogView.findViewById(R.id.mqtt_user_password);
+    }
+}
