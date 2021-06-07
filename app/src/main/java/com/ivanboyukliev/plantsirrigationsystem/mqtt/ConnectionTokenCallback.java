ed3263cb9ad362f4a8c475ddec94dadd5e873984
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-07 16:08:38 +0300

    Added separate handling for different broker errors.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
@@ -6,11 +6,15 @@ import android.util.Log;
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.impl.MqttClientActionsImpl;
+import com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttException;
+
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.AUTHORIZATION_ERROR;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.BROKER_CONNECTION_ERROR_MSG;
 
 public class ConnectionTokenCallback implements IMqttActionListener {
 
@@ -40,9 +44,14 @@ public class ConnectionTokenCallback implements IMqttActionListener {
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.i("BROKER INFO", "Connection failed!!");
         Log.i("REASON", exception.getMessage());
+        if (exception.getMessage().equals(AUTHORIZATION_ERROR)) {
+            HomeActivity.showBrokerError(AUTHORIZATION_ERROR);
+        }
+        if (exception.getMessage().equals(ApplicationConstants.BROKER_CONN_ERR)) {
+            HomeActivity.showBrokerError(BROKER_CONNECTION_ERROR_MSG);
+        }
         exception.printStackTrace();
         ((MqttClientActionsImpl) connectAction).setConnected(false);
-        HomeActivity.showBrokerConnectionError();
         HomeActivity.getBrokersAdapter().notifyDataSetChanged();
     }
 }
