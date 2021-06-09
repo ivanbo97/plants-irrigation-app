04735d32513bc5c847b042272ebd9e40ceacc072
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-09 19:29:33 +0300

    New function for validating the input value of broker port.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/UserInputValidator.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/UserInputValidator.java
@@ -1,7 +1,10 @@
 package com.ivanboyukliev.plantsirrigationsystem.utils;
 
 import android.util.Patterns;
+
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.MAX_PORT_NUMBER;
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.PASSWORD_PATTERN;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.PORT_PATTERN;
 
 
 public class UserInputValidator {
@@ -15,6 +18,13 @@ public class UserInputValidator {
 
     public static boolean isPasswordValid(String password) {
         if (password.isEmpty() || !PASSWORD_PATTERN.matcher(password).matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isPortValid(String port) {
+        if (!PORT_PATTERN.matcher(port).matches() || Integer.valueOf(port) > MAX_PORT_NUMBER) {
             return false;
         }
         return true;
