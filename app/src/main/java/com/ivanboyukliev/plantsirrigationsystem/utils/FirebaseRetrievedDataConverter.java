4d2f9adf92dc5b0ab371ef7b841d2bbc0c963cc7
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-10 18:47:58 +0300

    Utility class for converting broker url and topic name from firebase standard to common format.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/FirebaseRetrievedDataConverter.java
@@ -0,0 +1,14 @@
+package com.ivanboyukliev.plantsirrigationsystem.utils;
+
+public class FirebaseRetrievedDataConverter {
+
+    public static String convertBrokerTopicToStandard(String topicName){
+        return topicName.replace("(","/");
+    }
+
+    public static String convertServerURIToStandard(String serverURI){
+        return serverURI.replace("!", ":")
+                .replace(",", ".")
+                .replace("(", "/");
+    }
+}
