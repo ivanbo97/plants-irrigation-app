897b94868ed87a18efa395cfd6b1a2a54c9710ef
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-10 19:02:18 +0300

    Refactoring insertion of topic in DB: Preventing the insertion of topics with duplicate names. If topic with the same name exist but the value for QoS in different, only QoS value will be updated.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/dialogwindows/MqttBrokerShowTopicsDialog.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/dialogwindows/MqttBrokerShowTopicsDialog.java
@@ -8,19 +8,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
-import android.widget.EditText;
+
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
 import com.ivanboyukliev.plantsirrigationsystem.R;
 
@@ -28,8 +27,6 @@ import com.ivanboyukliev.plantsirrigationsystem.firebase.model.FirebaseTopicObj;
 import com.ivanboyukliev.plantsirrigationsystem.topicsrecyclerview.adapter.TopicsRecyclerViewListAdapter;
 
 import java.util.List;
-
-import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.DB_URL;
 
 public class MqttBrokerShowTopicsDialog extends AppCompatDialogFragment {
 
@@ -48,37 +45,21 @@ public class MqttBrokerShowTopicsDialog extends AppCompatDialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
+
         dialogView = inflater.inflate(R.layout.mqtt_broker_topics_dialog, null);
         topicsListRecyclerView = dialogView.findViewById(R.id.topicsListRecyclerView);
+
         topicsListRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayout.VERTICAL));
         LinearLayoutManager verticalLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         topicsListRecyclerView.setLayoutManager(verticalLayoutManager);
         currentBrokerTopics = HomeActivity.getMqttBrokersList().get(brokerNumber).getTopics();
         String brokerID = HomeActivity.getMqttBrokersList().get(brokerNumber).getBrokerID();
-        topicsAdapter = new TopicsRecyclerViewListAdapter(currentBrokerTopics,brokerID);
+        topicsAdapter = new TopicsRecyclerViewListAdapter(currentBrokerTopics, brokerID);
         topicsListRecyclerView.setAdapter(topicsAdapter);
         dialogBuilder.setView(dialogView)
                 .setTitle("Topics Subscription List")
                 .setNegativeButton("Add new topic", (dialog, which) -> {
-                    View topicDialogView = dialogView = inflater.inflate(R.layout.mqtt_broker_topic_reg, null);
-                    AlertDialog topicDialog = new AlertDialog.Builder(getActivity())
-                            .setView(topicDialogView)
-                            .setTitle("Enter topic")
-                            .setPositiveButton("Add", (dialog1, which1) -> {
-                                EditText newTopicEditText = topicDialogView.findViewById(R.id.mqtt_broker_new_topic);
-                                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                                DatabaseReference databaseBrokerTopics = FirebaseDatabase.getInstance(DB_URL).getReference(
-                                        "users" + "/" + firebaseAuth.getUid()
-                                                + "/" + brokerID + "/topics"
-                                );
-                                String pushID = databaseBrokerTopics.push().getKey();
-                                String newTopic = newTopicEditText.getText().toString();
-                                databaseBrokerTopics.child(pushID).setValue(newTopic);
-
-                            })
-                            .setNegativeButton("Cancel", null)
-                            .create();
-                    topicDialog.show();
+                    openTopicRegisterDialog(getParentFragmentManager());
                 });
         topicsAdapter.notifyDataSetChanged();
         return dialogBuilder.create();
@@ -88,5 +69,12 @@ public class MqttBrokerShowTopicsDialog extends AppCompatDialogFragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
     }
+
+    private void openTopicRegisterDialog(FragmentManager fragmentManager) {
+        String brokerID = HomeActivity.getMqttBrokersList().get(brokerNumber).getBrokerID();
+        MqttBrokerTopicRegDialog mqttBrokerTopicRegDialog = new MqttBrokerTopicRegDialog(brokerID);
+        mqttBrokerTopicRegDialog.show(fragmentManager, "MQTT Broker Registration");
+    }
+
 
 }
