ba852b135fd3cf92d54936c0777940cf9cc88dde
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-12 09:18:46 +0300

    Refactoring: topic data retrieving is extracted into new utility class.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/firebase/BrokerDataChangeListener.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/firebase/BrokerDataChangeListener.java
@@ -9,14 +9,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBrokerClient;
-import com.ivanboyukliev.plantsirrigationsystem.firebase.model.FirebaseTopicObj;
+import com.ivanboyukliev.plantsirrigationsystem.firebase.util.FirebaseDataExtractor;
 import com.ivanboyukliev.plantsirrigationsystem.utils.FirebaseRetrievedDataConverter;
 
 public class BrokerDataChangeListener implements ValueEventListener {
 
     @Override
     public void onDataChange(@NonNull DataSnapshot snapshot) {
-        HomeActivity.getMqttBrokersList().clear();
+
         for (DataSnapshot broker : snapshot.getChildren()) {
             BasicMqttBrokerClient mqttBroker = new BasicMqttBrokerClient();
             mqttBroker.setBrokerID(broker.getKey());
@@ -36,7 +36,7 @@ public class BrokerDataChangeListener implements ValueEventListener {
 
             mqttBroker.initClientData();
 
-            populateTopics(mqttBroker, broker.child("/topics").getChildren());
+            FirebaseDataExtractor.retrieveTopicsAndPopulateBroker(mqttBroker, broker.child("/topics").getChildren());
 
             HomeActivity.getMqttBrokersList().add(mqttBroker);
             HomeActivity.getBrokersAdapter().notifyDataSetChanged();
@@ -46,15 +46,7 @@ public class BrokerDataChangeListener implements ValueEventListener {
 
     @Override
     public void onCancelled(@NonNull DatabaseError error) {
-        Log.i("DB ERROR", error.toString());
+        HomeActivity.showBrokerMessage(error.getMessage());
     }
 
-    private void populateTopics(BasicMqttBrokerClient mqttBroker, Iterable<DataSnapshot> topics) {
-        for (DataSnapshot topic : topics) {
-            Integer QoS = topic.getValue(Integer.class);
-            String firebaseTopicName = topic.getKey();
-            String standardTopicName = FirebaseRetrievedDataConverter.convertBrokerTopicToStandard(firebaseTopicName);
-            mqttBroker.getTopics().add(new FirebaseTopicObj(standardTopicName, QoS));
-        }
-    }
 }
