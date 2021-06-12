57b2719e58b74fecd4f7a477c5f9c18651e4f790
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-12 09:30:36 +0300

    Deleting topic - new restriction - user cannot delete topic if connection to broker is lost. There is also a check for the state of the DB operation state.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/topicsrecyclerview/adapter/TopicsRecyclerViewListAdapter.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/topicsrecyclerview/adapter/TopicsRecyclerViewListAdapter.java
@@ -17,15 +17,19 @@ import com.ivanboyukliev.plantsirrigationsystem.utils.UserInputConverter;
 
 import java.util.List;
 
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.BROKER_DEL_ERROR;
+
 
 public class TopicsRecyclerViewListAdapter extends RecyclerView.Adapter<TopicViewHolder> {
 
     private List<FirebaseTopicObj> mqttBrokerTopics;
     private String brokerID;
+    private int brokerNumInList;
 
-    public TopicsRecyclerViewListAdapter(List<FirebaseTopicObj> mqttBrokerTopics, String brokerID) {
+    public TopicsRecyclerViewListAdapter(List<FirebaseTopicObj> mqttBrokerTopics, String brokerID, int brokerNumInList) {
         this.mqttBrokerTopics = mqttBrokerTopics;
         this.brokerID = brokerID;
+        this.brokerNumInList = brokerNumInList;
     }
 
     @NonNull
@@ -46,9 +50,16 @@ public class TopicsRecyclerViewListAdapter extends RecyclerView.Adapter<TopicVie
         holder.getTopicQoSTv().setText(qosTopicContent);
         holder.getDeleteTopicBtn().setOnClickListener(v -> {
             DatabaseReference currentTopicDB = HomeActivity.getmDatabaseAuthUserBrokers().child(brokerID + "/topics/" + firebaseTopicID);
-            currentTopicDB.removeValue();
-            mqttBrokerTopics.remove(position);
-            notifyDataSetChanged();
+            if (!HomeActivity.getMqttBrokersList().get(brokerNumInList).isConnected()) {
+                HomeActivity.showBrokerMessage(BROKER_DEL_ERROR);
+                return;
+            }
+            currentTopicDB.removeValue((error, ref) -> {
+                if (error == null) {
+                    mqttBrokerTopics.remove(position);
+                    notifyDataSetChanged();
+                }
+            });
         });
     }
 
