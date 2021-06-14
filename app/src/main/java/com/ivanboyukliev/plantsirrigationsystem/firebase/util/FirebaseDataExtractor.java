6d6fd9e2e3dcefafaee264a70834455e6f9d0b6d
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-14 16:35:42 +0300

    Added new method for plant data retrieval from broker node.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/firebase/util/FirebaseDataExtractor.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/firebase/util/FirebaseDataExtractor.java
@@ -1,7 +1,9 @@
 package com.ivanboyukliev.plantsirrigationsystem.firebase.util;
 
+
 import com.google.firebase.database.DataSnapshot;
 import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBrokerClient;
+import com.ivanboyukliev.plantsirrigationsystem.firebase.model.FirebasePlantObj;
 import com.ivanboyukliev.plantsirrigationsystem.firebase.model.FirebaseTopicObj;
 import com.ivanboyukliev.plantsirrigationsystem.utils.FirebaseRetrievedDataConverter;
 
@@ -13,6 +15,14 @@ public class FirebaseDataExtractor {
             String firebaseTopicName = topic.getKey();
             String standardTopicName = FirebaseRetrievedDataConverter.convertBrokerTopicToStandard(firebaseTopicName);
             mqttBroker.getTopics().add(new FirebaseTopicObj(standardTopicName, QoS));
+        }
+    }
+
+    public static void retrievePlantsAndPopulateBroker(BasicMqttBrokerClient mqttBroker, Iterable<DataSnapshot> plants) {
+        for (DataSnapshot plant : plants) {
+            String plantName = plant.getKey();
+            String imageURL = plant.getValue(String.class);
+            mqttBroker.getPlants().add(new FirebasePlantObj(plantName, imageURL));
         }
     }
 }
