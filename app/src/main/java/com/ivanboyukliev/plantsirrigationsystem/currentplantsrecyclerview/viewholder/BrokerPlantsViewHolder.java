32e9cc190a5192e3862dc8778d9a36966695a7e9
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-14 16:46:12 +0300

    View holder class for encapsulation of the widgets inside one plant row in plant list.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/currentplantsrecyclerview/viewholder/BrokerPlantsViewHolder.java
@@ -0,0 +1,50 @@
+package com.ivanboyukliev.plantsirrigationsystem.currentplantsrecyclerview.viewholder;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ivanboyukliev.plantsirrigationsystem.R;
+
+public class BrokerPlantsViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView plantNameTv;
+    private ImageView plantImage;
+    private FloatingActionButton deleteButton;
+
+    public BrokerPlantsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        plantNameTv = itemView.findViewById(R.id.plantNameCardView);
+        plantImage = itemView.findViewById(R.id.plantImageView);
+        deleteButton = itemView.findViewById(R.id.deletePlantBtn);
+    }
+
+    public TextView getPlantNameTv() {
+        return plantNameTv;
+    }
+
+    public void setPlantNameTv(TextView plantNameTv) {
+        this.plantNameTv = plantNameTv;
+    }
+
+    public ImageView getPlantImage() {
+        return plantImage;
+    }
+
+    public void setPlantImage(ImageView plantImage) {
+        this.plantImage = plantImage;
+    }
+
+    public FloatingActionButton getDeleteButton() {
+        return deleteButton;
+    }
+
+    public void setDeleteButton(FloatingActionButton deleteButton) {
+        this.deleteButton = deleteButton;
+    }
+}
