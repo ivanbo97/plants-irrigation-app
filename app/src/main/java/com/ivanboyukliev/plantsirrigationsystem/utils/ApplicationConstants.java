26a0a7f2fe7c714ed55cd9b617cf8802b78a322a
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-15 18:51:40 +0300

    New constant string for widgets inside alert boxes dedicated to deletion of items.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/ApplicationConstants.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/utils/ApplicationConstants.java
@@ -75,6 +75,7 @@ public final class ApplicationConstants {
     public static final String PLANT_REG_ERROR = "Connection to Broker is lost! Cannot add new plant!";
     public static final String PLANT_INFO = "Tap on plant's name for more..";
 
-
-
+    public static final String DELETE_TOPIC_DIALOG_TITLE = "Do you want to delete this item from DB?";
+    public static final String DEL_TOPIC_BTN_TXT = "Delete";
+    public static final String DEL_ITEM_DIALOG_TITLE = "Item Deletion";
 }
