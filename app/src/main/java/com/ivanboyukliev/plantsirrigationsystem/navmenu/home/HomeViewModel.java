8e4a6ccc9c35bbca54675dba89f400fb7aa2dc3a
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-17 17:21:50 +0300

    New class member encapsulating plant's data.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/home/HomeViewModel.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/home/HomeViewModel.java
@@ -1,19 +1,39 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.home;
 
-import androidx.lifecycle.LiveData;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.ivanboyukliev.plantsirrigationsystem.searchedplantsrecyclerview.model.PlantFromApi;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<PlantFromApi> currentPlant;
+
+    private MutableLiveData<String> plantFamily;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        currentPlant = new MutableLiveData<>();
+        retrievePlantInfo();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<PlantFromApi> getPlantScientificName() {
+        return currentPlant;
+    }
+
+    public MutableLiveData<String> getPlantFamily() {
+        return plantFamily;
+    }
+
+    public void setPlantFamily(MutableLiveData<String> plantFamily) {
+        this.plantFamily = plantFamily;
+    }
+
+    public MutableLiveData<PlantFromApi> getCurrentPlant() {
+        return currentPlant;
+    }
+
+    public void retrievePlantInfo() {
+        HomeFragment.getPlantApiRequest().getSinglePlantData(currentPlant);
     }
 }
