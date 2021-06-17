f05101c4a444f6c1d584419124a2516384fdf1c6
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-17 17:20:52 +0300

    Code reformatting.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/PlantManagerActivity.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/PlantManagerActivity.java
@@ -1,6 +1,7 @@
 package com.ivanboyukliev.plantsirrigationsystem;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -20,7 +21,7 @@ public class PlantManagerActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.plant_irrigation, R.id.realtime_data,R.id.irrigation_history)
+                R.id.navigation_home, R.id.plant_irrigation, R.id.realtime_data, R.id.irrigation_history)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
