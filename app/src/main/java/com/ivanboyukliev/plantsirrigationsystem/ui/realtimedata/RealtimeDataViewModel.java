21bd86be61c8b7559b3ad022c377ee5739b4d7a6
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-17 10:20:10 +0300

    Fragment for realtime data view.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/ui/realtimedata/RealtimeDataViewModel.java
@@ -0,0 +1,19 @@
+package com.ivanboyukliev.plantsirrigationsystem.ui.realtimedata;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RealtimeDataViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public RealtimeDataViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is realtime data fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
