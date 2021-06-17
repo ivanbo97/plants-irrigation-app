b0b712e5082a40cc0468ac9efd6800e1c75a1ce7
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-17 17:23:05 +0300

    Added an observer for handling changes in plant's data.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/home/HomeFragment.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/home/HomeFragment.java
@@ -1,36 +1,67 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.ivanboyukliev.plantsirrigationsystem.R;
+import com.ivanboyukliev.plantsirrigationsystem.plantapi.PlantApiRequest;
+import com.ivanboyukliev.plantsirrigationsystem.searchedplantsrecyclerview.model.PlantFromApi;
+import com.squareup.picasso.Picasso;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private static String plantName;
+
+    private TextView plantScientificName;
+    private ImageView plantImageView;
+
+    private static PlantApiRequest plantApiRequest;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        Intent parentIntent = getActivity().getIntent();
+
+        plantName = parentIntent.getExtras().getString("PlantName");
+
+        plantApiRequest = new PlantApiRequest(plantName, getContext());
+
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+
+        plantScientificName = root.findViewById(R.id.plantImageHomeMenu);
+
+        plantImageView = root.findViewById(R.id.plantImageHomeMenu);
+
+        plantScientificName = root.findViewById(R.id.plantScientificName);
+
+        homeViewModel.getCurrentPlant().observe(getViewLifecycleOwner(), plantFromApi -> {
+            plantScientificName.setText(plantFromApi.getScientificName());
+            Picasso.get().load(plantFromApi.getImageUrl())
+                    .resize(160, 174)
+                    .into(plantImageView);
         });
+
         return root;
+    }
+
+    public static String getPlantName() {
+        return plantName;
+    }
+
+    public static PlantApiRequest getPlantApiRequest() {
+        return plantApiRequest;
     }
 }
