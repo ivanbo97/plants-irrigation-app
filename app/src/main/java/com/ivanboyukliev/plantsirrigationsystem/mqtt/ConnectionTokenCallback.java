70770112c5e86342c78477109a8d042c30a81b98
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-18 17:44:37 +0300

    After successful connection - subscription to current topics can be made. Also added method call for keeping HomeFragment connection state to broker consistent.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/ConnectionTokenCallback.java
@@ -1,56 +1,46 @@
 package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 
-
 import android.util.Log;
 
-import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
-import com.ivanboyukliev.plantsirrigationsystem.brokersrecyclerview.model.BasicMqttBrokerClient;
 import com.ivanboyukliev.plantsirrigationsystem.mqtt.api.MqttClientActions;
-
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.home.HomeFragment;
 import com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttException;
 
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.AUTHORIZATION_ERROR;
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.BROKER_CONNECTION_ERROR_MSG;
 
 public class ConnectionTokenCallback implements IMqttActionListener {
 
-    private MqttClientActions connectAction;
+    private MqttClientActions clientActions;
     private MqttAndroidClient mqttAndroidClient;
 
-    public ConnectionTokenCallback(MqttClientActions connectAction, MqttAndroidClient androidClient) {
-        this.connectAction = connectAction;
+    public ConnectionTokenCallback(MqttClientActions clientActions, MqttAndroidClient androidClient) {
+        this.clientActions = clientActions;
         this.mqttAndroidClient = androidClient;
     }
 
     @Override
     public void onSuccess(IMqttToken asyncActionToken) {
         Log.i("BROKER INFO", "Connection success!!");
-        HomeActivity.getBrokersAdapter().notifyDataSetChanged();
-        TopicSubscriptionCallBack topicSubscriptionCallBack = new TopicSubscriptionCallBack();
-        try {
-            mqttAndroidClient.subscribe("house/bulb1", 0, null, topicSubscriptionCallBack);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
+        clientActions.subscribeToTopics();
     }
 
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.i("BROKER INFO", "Connection failed!!");
         Log.i("REASON", exception.getMessage());
+        HomeFragment.getConnectAndSubsBtn().setEnabled(true);
+        HomeFragment.getBrokerDisconnectBtn().setEnabled(false);
         if (exception.getMessage().equals(AUTHORIZATION_ERROR)) {
-            HomeActivity.showBrokerMessage(AUTHORIZATION_ERROR);
+            HomeFragment.showBrokerMessage(AUTHORIZATION_ERROR);
         }
         if (exception.getMessage().equals(ApplicationConstants.BROKER_CONN_ERR)) {
-            HomeActivity.showBrokerMessage(BROKER_CONNECTION_ERROR_MSG);
+            HomeFragment.showBrokerMessage(BROKER_CONNECTION_ERROR_MSG);
         }
         exception.printStackTrace();
-        HomeActivity.getBrokersAdapter().notifyDataSetChanged();
     }
 }
