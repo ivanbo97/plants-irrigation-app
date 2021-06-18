8c2bc5f47b388bc3484be6557d326829d5671503
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-18 17:31:14 +0300

    Added logic for handling connect and disconnect buttons states (enabled/disabled).

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/TopicSubscriptionCallBack.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/TopicSubscriptionCallBack.java
@@ -2,6 +2,8 @@ package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 
 import android.util.Log;
 
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.home.HomeFragment;
+
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 
@@ -9,11 +11,20 @@ public class TopicSubscriptionCallBack implements IMqttActionListener {
 
     @Override
     public void onSuccess(IMqttToken asyncActionToken) {
-        Log.i("Mqtt","Subscribed!");
+        Log.i("Mqtt", "Subscribed!");
+        HomeFragment.showBrokerMessage("Connection to broker and topic subscription successful!");
+        HomeFragment.getConnectAndSubsBtn().setEnabled(false);
+        HomeFragment.getBrokerDisconnectBtn().setEnabled(true);
+        HomeFragment.setConnectedToBroker(true);
     }
 
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.w("Mqttt", "Subscription failed!");
+        HomeFragment.showBrokerMessage("Successful broker connection! Failure during topic subscription!");
+        HomeFragment.getMqttClient().disconnectClient();
+        HomeFragment.getConnectAndSubsBtn().setEnabled(true);
+        HomeFragment.getBrokerDisconnectBtn().setEnabled(false);
+        HomeFragment.setConnectedToBroker(false);
     }
 }
