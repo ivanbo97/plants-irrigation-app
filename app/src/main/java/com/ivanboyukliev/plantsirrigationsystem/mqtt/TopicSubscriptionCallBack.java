292aaaf4992aa63e90e7d17edb28091fa3282dbd
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-19 07:34:55 +0300

    Refactored hardcoded strings with string constants.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/TopicSubscriptionCallBack.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/TopicSubscriptionCallBack.java
@@ -7,12 +7,15 @@ import com.ivanboyukliev.plantsirrigationsystem.navmenu.home.HomeFragment;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.BROKER_TOPICS_SUBS_ERROR;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.BROKER_TOPICS_SUBS_SUCCESS;
+
 public class TopicSubscriptionCallBack implements IMqttActionListener {
 
     @Override
     public void onSuccess(IMqttToken asyncActionToken) {
         Log.i("Mqtt", "Subscribed!");
-        HomeFragment.showBrokerMessage("Connection to broker and topic subscription successful!");
+        HomeFragment.showBrokerMessage(BROKER_TOPICS_SUBS_SUCCESS);
         HomeFragment.getConnectAndSubsBtn().setEnabled(false);
         HomeFragment.getBrokerDisconnectBtn().setEnabled(true);
         HomeFragment.setConnectedToBroker(true);
@@ -21,7 +24,7 @@ public class TopicSubscriptionCallBack implements IMqttActionListener {
     @Override
     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
         Log.w("Mqttt", "Subscription failed!");
-        HomeFragment.showBrokerMessage("Successful broker connection! Failure during topic subscription!");
+        HomeFragment.showBrokerMessage(BROKER_TOPICS_SUBS_ERROR);
         HomeFragment.getMqttClient().disconnectClient();
         HomeFragment.getConnectAndSubsBtn().setEnabled(true);
         HomeFragment.getBrokerDisconnectBtn().setEnabled(false);
