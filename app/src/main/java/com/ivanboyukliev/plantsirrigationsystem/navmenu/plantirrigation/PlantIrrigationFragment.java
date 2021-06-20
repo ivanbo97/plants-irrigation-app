28cdbdba1bb3bc929219923213baed77902e507f
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-20 11:44:29 +0300

    Added fragment for testing if messages received from broker are correctly displayed.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/PlantIrrigationFragment.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/PlantIrrigationFragment.java
@@ -12,7 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ivanboyukliev.plantsirrigationsystem.PlantManagerActivity;
 import com.ivanboyukliev.plantsirrigationsystem.R;
+import com.ivanboyukliev.plantsirrigationsystem.mqtt.AndroidMqttClientCallback;
 
 public class PlantIrrigationFragment extends Fragment {
 
@@ -25,6 +27,13 @@ public class PlantIrrigationFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_irrigation, container, false);
         final TextView textView = root.findViewById(R.id.text_irrigation);
         plantIrrigationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        ((AndroidMqttClientCallback) PlantManagerActivity.getMqttClient().getMqttCallback()).getReceivedData().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
