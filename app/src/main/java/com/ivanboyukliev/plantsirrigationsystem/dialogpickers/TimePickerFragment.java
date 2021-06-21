3dc44d631e0f4a6b71c6293e6874ea82e316fd9b
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-21 17:41:19 +0300

    Fragment for displaying time picker dialog.

---
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/dialogpickers/TimePickerFragment.java
@@ -0,0 +1,34 @@
+package com.ivanboyukliev.plantsirrigationsystem.dialogpickers;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import androidx.fragment.app.DialogFragment;
+
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.PlantIrrigationFragment;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+        PlantIrrigationFragment.getInputTimeTv().setText(hourOfDay + ":" + minute);
+    }
+}
