c162f8e45fc9b2eb640e8a34bbb87de2463a38b7
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-21 09:54:34 +0300

    Class should be empty for now.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/PlantIrrigationViewModel.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/PlantIrrigationViewModel.java
@@ -1,19 +1,13 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PlantIrrigationViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> brokerConnState;
 
     public PlantIrrigationViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is irrigation fragment");
-    }
 
-    public LiveData<String> getText() {
-        return mText;
     }
 }
