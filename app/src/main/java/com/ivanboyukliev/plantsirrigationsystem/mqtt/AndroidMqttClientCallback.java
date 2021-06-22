b24f6682e702ca3f65aa3739671bc733b53908b0
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-22 16:15:09 +0300

    Making messages received from broker MutableLiveData objects.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/AndroidMqttClientCallback.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/mqtt/AndroidMqttClientCallback.java
@@ -2,7 +2,6 @@ package com.ivanboyukliev.plantsirrigationsystem.mqtt;
 
 import android.util.Log;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.ivanboyukliev.plantsirrigationsystem.HomeActivity;
@@ -14,15 +13,26 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.DELAYED_START_STATE_TOPIC;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.MAINTAIN_MOISTURE_TASK_STATE_TOPIC;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.MOISTURE_LEVEL_TOPIC;
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.PUMP_STATE_TOPIC;
+
 public class AndroidMqttClientCallback implements MqttCallbackExtended {
 
     private MqttClientActions disconnectAction;
 
-    private MutableLiveData<String> receivedData;
+    private MutableLiveData<String> receivedMoisture;
+    private MutableLiveData<String> receivedPumpState;
+    private MutableLiveData<String> delayedIrrigationState;
+    private MutableLiveData<String> moistureMaintainingOperationState;
 
     public AndroidMqttClientCallback(MqttClientActions disconnectAction) {
         this.disconnectAction = disconnectAction;
-        receivedData = new MutableLiveData<>();
+        receivedMoisture = new MutableLiveData<>();
+        receivedPumpState = new MutableLiveData<>();
+        delayedIrrigationState = new MutableLiveData<>();
+        moistureMaintainingOperationState = new MutableLiveData<>();
     }
 
     @Override
@@ -44,7 +54,43 @@ public class AndroidMqttClientCallback implements MqttCallbackExtended {
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
         Log.i("DATA FROM BROKER", message.toString());
-        receivedData.setValue(message.toString());
+
+        if (topic.equals(PUMP_STATE_TOPIC)) {
+            Log.i("PUMP STATE", message.toString());
+            receivedPumpState.setValue(message.toString());
+            return;
+        }
+
+        if (topic.equals(MOISTURE_LEVEL_TOPIC)) {
+            receivedMoisture.setValue(message.toString());
+            return;
+        }
+
+        if (topic.equals(DELAYED_START_STATE_TOPIC)) {
+            delayedIrrigationState.setValue(message.toString());
+            return;
+        }
+
+        if (topic.equals(MAINTAIN_MOISTURE_TASK_STATE_TOPIC)) {
+            moistureMaintainingOperationState.setValue(message.toString());
+            return;
+        }
+    }
+
+    public MutableLiveData<String> getReceivedMoisture() {
+        return receivedMoisture;
+    }
+
+    public MutableLiveData<String> getReceivedPumpState() {
+        return receivedPumpState;
+    }
+
+    public MutableLiveData<String> getDelayedIrrigationState() {
+        return delayedIrrigationState;
+    }
+
+    public MutableLiveData<String> getMoistureMaintainingOperationState() {
+        return moistureMaintainingOperationState;
     }
 
     @Override
@@ -52,7 +98,5 @@ public class AndroidMqttClientCallback implements MqttCallbackExtended {
 
     }
 
-    public LiveData<String> getReceivedData() {
-        return receivedData;
-    }
+
 }
