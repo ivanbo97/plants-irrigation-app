ad329608aca523cb2ac75e04f612669186995801
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-23 17:58:28 +0300

    Keeping start and terminate buttons consistent - when start has been clicked, it should be made inactive and afterwards terminate button should be activated.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/widgetslisteners/MaintainMoistureBtnListener.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/widgetslisteners/MaintainMoistureBtnListener.java
@@ -1,13 +1,14 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.widgetslisteners;
 
-import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
 import com.ivanboyukliev.plantsirrigationsystem.PlantManagerActivity;
 import com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.PlantIrrigationFragment;
-import com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.utils.IrrigationSystemState;
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.utils.MoistureManagementWidgets;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
@@ -17,33 +18,27 @@ import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstant
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.ERROR_PUBLISH_MESSAGE;
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.MAINTAIN_MOISTURE_VALUE_TOPIC;
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.MOISTURE_MAINTAIN_FLAG;
-import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.PUMP_BUSY_MSG;
 import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.SUCCESSFUL_MESSAGE_PUBLISH;
 
 public class MaintainMoistureBtnListener implements View.OnClickListener {
 
-    private IrrigationSystemState currentSystemState;
-    private Context activityContext;
 
-    public MaintainMoistureBtnListener(IrrigationSystemState irrigationSystemState, Context context) {
-        this.currentSystemState = irrigationSystemState;
-        this.activityContext = context;
+    private Fragment parentFragment;
+
+    public MaintainMoistureBtnListener(PlantIrrigationFragment parentFragment) {
+        this.parentFragment = parentFragment;
     }
 
     @Override
     public void onClick(View v) {
 
-        String desiredMoisture = PlantIrrigationFragment.getEnteredMoisture().getText().toString();
+        String desiredMoisture = MoistureManagementWidgets.getEnteredMoisture().getText().toString();
         Log.i("DESIRED MOISTURE", desiredMoisture);
         if (desiredMoisture.equals("")) {
-            Toast.makeText(activityContext, EMPTY_MOISTURE_FIELD, Toast.LENGTH_SHORT).show();
+            Toast.makeText(parentFragment.getContext(), EMPTY_MOISTURE_FIELD, Toast.LENGTH_SHORT)
+                    .show();
             return;
         }
-        if (currentSystemState.isPumpActive() || currentSystemState.isMoistureMaintainTaskOn() || currentSystemState.isDelayedStartTaskOn()) {
-            Toast.makeText(activityContext, PUMP_BUSY_MSG, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
 
         MqttMessage initMoistureTask = new MqttMessage();
         initMoistureTask.setPayload(MOISTURE_MAINTAIN_FLAG.getBytes());
@@ -56,10 +51,12 @@ public class MaintainMoistureBtnListener implements View.OnClickListener {
         try {
             PlantManagerActivity.getMqttClient().getMqttAndroidClient().publish(ACTIVATE_MAINTAIN_MOISTURE_TOPIC, initMoistureTask);
             PlantManagerActivity.getMqttClient().getMqttAndroidClient().publish(MAINTAIN_MOISTURE_VALUE_TOPIC, moistureValue);
-            Toast.makeText(activityContext, SUCCESSFUL_MESSAGE_PUBLISH + ACTIVATE_MAINTAIN_MOISTURE_TOPIC +
+            MoistureManagementWidgets.getTerminateMoistureTaskBtn().setEnabled(true);
+            Toast.makeText(parentFragment.getContext(), SUCCESSFUL_MESSAGE_PUBLISH + ACTIVATE_MAINTAIN_MOISTURE_TOPIC +
                     "," + MAINTAIN_MOISTURE_VALUE_TOPIC, Toast.LENGTH_LONG).show();
+            v.setEnabled(false);
         } catch (MqttException e) {
-            Toast.makeText(activityContext, ERROR_PUBLISH_MESSAGE + ACTIVATE_MAINTAIN_MOISTURE_TOPIC + "/"
+            Toast.makeText(parentFragment.getContext(), ERROR_PUBLISH_MESSAGE + ACTIVATE_MAINTAIN_MOISTURE_TOPIC + "/"
                     + MAINTAIN_MOISTURE_VALUE_TOPIC, Toast.LENGTH_LONG).show();
 
             e.printStackTrace();
