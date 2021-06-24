91b4d8e14fb9142437d7f3be92863b60031158e4
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-24 15:02:30 +0300

    Class notifies whenever there is a setter is called on any of its members.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/utils/IrrigationSystemState.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/plantirrigation/utils/IrrigationSystemState.java
@@ -1,38 +1,60 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.plantirrigation.utils;
 
-public class IrrigationSystemState {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.library.baseAdapters.BR;
+
+public class IrrigationSystemState extends BaseObservable {
 
     private boolean pumpTaskRunning;
     private boolean moistureMaintainTaskRunning;
     private boolean delayedStartTaskRunning;
+    private boolean connectedToBroker;
 
     public IrrigationSystemState() {
         pumpTaskRunning = false;
         moistureMaintainTaskRunning = false;
         delayedStartTaskRunning = false;
+        connectedToBroker = false;
     }
 
+    @Bindable
     public boolean isPumpTaskRunning() {
         return pumpTaskRunning;
     }
 
     public void setPumpTaskRunning(boolean pumpTaskRunning) {
         this.pumpTaskRunning = pumpTaskRunning;
+        notifyPropertyChanged(BR.pumpTaskRunning);
     }
 
+    @Bindable
     public boolean isMoistureMaintainTaskRunning() {
         return moistureMaintainTaskRunning;
     }
 
     public void setMoistureMaintainTaskRunning(boolean moistureMaintainTaskRunning) {
         this.moistureMaintainTaskRunning = moistureMaintainTaskRunning;
+        notifyPropertyChanged(BR.moistureMaintainTaskRunning);
     }
 
+    @Bindable
     public boolean isDelayedStartTaskRunning() {
         return delayedStartTaskRunning;
     }
 
     public void setDelayedStartTaskRunning(boolean delayedStartTaskRunning) {
         this.delayedStartTaskRunning = delayedStartTaskRunning;
+        notifyPropertyChanged(BR.delayedStartTaskRunning);
+    }
+
+    @Bindable
+    public boolean isConnectedToBroker() {
+        return connectedToBroker;
+    }
+
+    public void setConnectedToBroker(boolean connectedToBroker) {
+        this.connectedToBroker = connectedToBroker;
+        notifyPropertyChanged(BR.connectedToBroker);
     }
 }
