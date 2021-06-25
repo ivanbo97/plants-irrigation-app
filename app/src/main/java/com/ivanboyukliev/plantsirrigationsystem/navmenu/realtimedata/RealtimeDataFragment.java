1a7955a5e769f46b8f07983ef46364a94a92a1dd
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-25 13:35:23 +0300

    Integrating chart for displaying live soil moisture data.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/realtimedata/RealtimeDataFragment.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/realtimedata/RealtimeDataFragment.java
@@ -4,32 +4,86 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.ivanboyukliev.plantsirrigationsystem.PlantManagerActivity;
 import com.ivanboyukliev.plantsirrigationsystem.R;
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.realtimedata.datachart.MoistureDataChart;
 
 public class RealtimeDataFragment extends Fragment {
 
     private RealtimeDataViewModel realtimeDataViewModel;
+    private LineChart lineChart;
+    private LineData moistureData;
+    private MoistureDataChart moistureChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         realtimeDataViewModel =
                 new ViewModelProvider(this).get(RealtimeDataViewModel.class);
+
+        moistureData = PlantManagerActivity.getMoistureChartData();
+
         View root = inflater.inflate(R.layout.fragment_realtime, container, false);
-        final TextView textView = root.findViewById(R.id.text_realtime);
-        realtimeDataViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        TextView connStateTv = root.findViewById(R.id.connStatusTvRealtime);
+        TextView runningTaskTv = root.findViewById(R.id.runningTaskTv);
+        TextView observedPlantTv = root.findViewById(R.id.observedPlantTv);
+
+        TextView currentTemperatureTv = root.findViewById(R.id.currentTempTv);
+        TextView currentMoistureTv = root.findViewById(R.id.currentMoistureTv);
+        TextView currentSeasonTv = root.findViewById(R.id.currentSeasonTv);
+
+        lineChart = root.findViewById(R.id.moistureChart);
+        moistureChart = new MoistureDataChart(lineChart);
+        moistureChart.setLineData(moistureData);
+
+        realtimeDataViewModel.getBrokerConnState().observe(getViewLifecycleOwner(), connected -> {
+            if (connected) {
+                connStateTv.setText("Connected");
+
+                return;
+            }
+            connStateTv.setText("Disconnected");
+            clearLiveDataWidgetsContent();
+            runningTaskTv.setText("");
+        });
+
+        realtimeDataViewModel.getMoistureValue().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(String moistureValue) {
+                try {
+                    moistureChart.addEntry(Float.valueOf(moistureValue));
+                    currentMoistureTv.setText(moistureValue + " %");
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getContext(), "Received a corrupted moisture value!", Toast.LENGTH_LONG);
+                    e.printStackTrace();
+                }
             }
         });
         return root;
+    }
+
+    private void clearLiveDataWidgetsContent() {
+        RelativeLayout liveDataRelLayout = getActivity().findViewById(R.id.liveDataLayout);
+        for (int i = 0; i < liveDataRelLayout.getChildCount(); i++) {
+            TextView textView = (TextView) liveDataRelLayout.getChildAt(i);
+            int viewId = textView.getId();
+            //Prevents clearing Live Data title
+            if (viewId == R.id.receivedDataFromBrokerTitle)
+                continue;
+            textView.setText("");
+        }
     }
 }
