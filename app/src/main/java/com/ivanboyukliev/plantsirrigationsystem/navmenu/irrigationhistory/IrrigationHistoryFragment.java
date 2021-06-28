6ca088377afc18ddb18f85a66d81282c2ed0e60c
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-28 15:24:14 +0300

    Recycler view was added to design.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/irrigationhistory/IrrigationHistoryFragment.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/irrigationhistory/IrrigationHistoryFragment.java
@@ -4,34 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ivanboyukliev.plantsirrigationsystem.R;
 
-
 public class IrrigationHistoryFragment extends Fragment {
 
-    private  IrrigationHistoryViewModel irrigationHistoryViewModel;
+    private IrrigationHistoryViewModel irrigationHistoryViewModel;
+
+    private RecyclerView irrigationRecordsRecyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         irrigationHistoryViewModel =
                 new ViewModelProvider(this).get(IrrigationHistoryViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_irrigation_history, container, false);
-        final TextView textView = root.findViewById(R.id.text_history);
-        irrigationHistoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        irrigationRecordsRecyclerView = root.findViewById(R.id.irrigationsListRecyclerView);
+        irrigationRecordsRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayout.VERTICAL));
+        LinearLayoutManager verticalLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        irrigationRecordsRecyclerView.setLayoutManager(verticalLayoutManager);
+        irrigationRecordsRecyclerView.setAdapter(IrrigationHistoryViewModel.getIrrigationRecordsAdapter());
         return root;
     }
 }
