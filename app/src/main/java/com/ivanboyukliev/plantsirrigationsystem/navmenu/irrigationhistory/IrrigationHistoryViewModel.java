0737a73a97d432b9385458c9d7dc3f0ae91637ec
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-06-28 15:23:42 +0300

    Retrieving data from Firebase related to irrigation records.

--- app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/irrigationhistory/IrrigationHistoryViewModel.java
+++ app/src/main/java/com/ivanboyukliev/plantsirrigationsystem/navmenu/irrigationhistory/IrrigationHistoryViewModel.java
@@ -1,19 +1,54 @@
 package com.ivanboyukliev.plantsirrigationsystem.navmenu.irrigationhistory;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class IrrigationHistoryViewModel extends ViewModel {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ivanboyukliev.plantsirrigationsystem.PlantManagerActivity;
+import com.ivanboyukliev.plantsirrigationsystem.firebase.IrrigationRecordsListener;
+import com.ivanboyukliev.plantsirrigationsystem.firebase.model.FirebaseIrrigationRecord;
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.home.HomeFragment;
+import com.ivanboyukliev.plantsirrigationsystem.navmenu.irrigationhistory.irrigationsrecyclerview.adapter.IrrigationRecordsListAdapter;
+import com.ivanboyukliev.plantsirrigationsystem.utils.UserInputConverter;
 
-    private MutableLiveData<String> mText;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.ivanboyukliev.plantsirrigationsystem.utils.ApplicationConstants.DB_URL;
+
+public class IrrigationHistoryViewModel extends ViewModel {
+    private static List<FirebaseIrrigationRecord> irrigationRecords;
+    private static IrrigationRecordsListAdapter irrigationRecordsAdapter;
 
     public IrrigationHistoryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is irrigation history fragment");
+
+        irrigationRecords = new ArrayList<>();
+
+        irrigationRecordsAdapter = new IrrigationRecordsListAdapter(irrigationRecords);
+
+
+        String standardBrokerUrl = PlantManagerActivity.getMqttClient().getBrokerUri();
+        String firebaseBrokerUrl = UserInputConverter.convertServerURIToFirebaseRules(standardBrokerUrl);
+        String currentPlantName = HomeFragment.getPlantName();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        DatabaseReference currentPlantIrrigationRecords = FirebaseDatabase.getInstance(DB_URL)
+                .getReference("users/" +
+                        firebaseAuth.getUid()
+                        + "/brokers/" +
+                        firebaseBrokerUrl +
+                        "/irrigations/" +
+                        currentPlantName);
+
+        currentPlantIrrigationRecords.addValueEventListener(new IrrigationRecordsListener());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public static List<FirebaseIrrigationRecord> getIrrigationRecords() {
+        return irrigationRecords;
+    }
+
+    public static IrrigationRecordsListAdapter getIrrigationRecordsAdapter() {
+        return irrigationRecordsAdapter;
     }
 }
